6295dccd1731bc41563ee24c50cf1a0fa6e12a54
westernavenue <141945337+Willwesternavenue@users.noreply.github.com>
2024-04-14 08:03:53 +0900

    403 fix

--- zScope/src/App.js
+++ zScope/src/App.js
@@ -1,11 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+
+// credential.json をインポート
+//import credential from './credential.json';
 
 function App() {
   const [mySign, setMySign] = useState('');
   const [partnerSign, setPartnerSign] = useState('');
   const [compatibility, setCompatibility] = useState(null);
-  const [error, setError] = useState(''); // エラーメッセージの状態を追加
+  const [error, setError] = useState(''); 
+
+  useEffect(() => {
+    calculateCompatibility(); // コンポーネントがマウントされたときにデータを取得する
+  }, []); // 空の依存リストを渡すことで、コンポーネントがマウントされたときにのみ呼び出されるようにする
 
   const handleSignChange = (event, setter) => {
     setter(event.target.value);
@@ -14,27 +21,32 @@ function App() {
   const calculateCompatibility = async () => {
     setError(''); // エラー状態をリセット
     try {
-      const apiKey = process.env.REACT_APP_GOOGLE_SHEETS_API_KEY_BASE64;
-      const apiKeyDecoded = atob(apiKey); // Base64デコード
-  
-      const spreadsheetId = "1_ny_jpo6gXKmSTMTaQT1o6RdEKR7UzrLA82aFc_1HV8"; // 実際のスプレッドシートIDを設定
-      const response = await fetch(`https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/Sheet1!A2:C13?key=${apiKeyDecoded}`);
+      const apiKey = 'AIzaSyBdSdUJ2SnudvSs0FTYe2aLugIYToCvLOU';
+      const spreadsheetId = '1_ny_jpo6gXKmSTMTaQT1o6RdEKR7UzrLA82aFc_1HV8';
+
+      const response = await fetch(`https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/Sheet1!A2:C13?key=${apiKey}`);
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
       }
       const data = await response.json();
       const values = data.values;
-  
-      // ここでデータを使用するコード
+      const matchingRow = values.find(row => row[0] === mySign && row[1] === partnerSign);
+      if (matchingRow) {
+        setCompatibility(matchingRow[2]); // Chemistry値を設定
+      } else {
+        setCompatibility('No data available'); // マッチするデータがない場合
+      }
     } catch (error) {
-      console.error('Error fetching data: ', error);
-      setError(error.message); // エラーメッセージを状態に保存
+      console.error('Error fetching data:', error);
+      setError('Failed to load data.'); // エラー状態を設定
     }
   };
+  
+  //console.log('API Key Base64:', process.env.REACT_APP_GOOGLE_SHEETS_API_KEY_BASE64);
 
   return (
     <div className="App">
-      <h1>星座の相性チェッカー</h1>
+      <h1>星座相性チェッカー</h1>
       <div>
         <label>あなたの星座：</label>
         <select value={mySign} onChange={(e) => handleSignChange(e, setMySign)}>
@@ -68,3 +80,4 @@ function App() {
 }
 
 export default App;
+ 
